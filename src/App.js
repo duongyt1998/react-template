a3d6a0e6a7d0abddb459d1ff07df73c0b4bf9217
duong-orai <duong.nt@orai.io>
2021-11-13 16:00:18 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { light } from "src/themes/light";
+import Header from "./components/Header";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
+import { useWeb3React } from "@web3-react/core";
+import { useEagerConnect, useInactiveListener } from "src/helpers/hooks";
+import Home from "./pages/Home";
+import Messages from "./components/Messages";
 
 function App() {
+  const context = useWeb3React();
+  const { connector } = context;
+  const [activatingConnector, setActivatingConnector] = useState();
+  const triedEager = useEagerConnect();
+  useInactiveListener(!triedEager || !!activatingConnector);
+  useEffect(() => {
+    if (activatingConnector && activatingConnector === connector) {
+      setActivatingConnector(undefined);
+    }
+  }, [activatingConnector, connector]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <ThemeProvider theme={light}>
+        <CssBaseline />
+        <Messages />
+        <Router>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </Router>
+      </ThemeProvider>
+    </Provider>
   );
 }
 
